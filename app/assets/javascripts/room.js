7444048cbe0b5efe345790ae8ec593014f25e51a
vishnugudali <65816781+vishnugudali@users.noreply.github.com>
2023-03-26 13:28:37 +0530

    CNX-avalon new room access code mandate.

--- app/assets/javascripts/room.js
+++ app/assets/javascripts/room.js
@@ -28,6 +28,7 @@ $(document).on('turbolinks:load', function(){
     // Display and update all fields related to creating a room in the createRoomModal
     $("#create-room-block").click(function(){
       showCreateRoom(this)
+      generateAccessCode()
     })
 
     checkIfAutoJoin()
